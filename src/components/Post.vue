130b4c98150cafdd88e66e4c18a25f3c5603bac8
Travis Lindsey <reachjtl@gmail.com>
2021-10-27 16:19:27 -0400

    initial commit

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -6,13 +6,21 @@
           class="col-12 col-md-6"
           max-height="250px"
           src="https://images.unsplash.com/photo-1518895949257-7621c3c786d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=688&q=80"
-        ></v-img> 
+        ></v-img>
 
         <v-col class="col-12 col-md-6">
           <div>Post Title Example</div>
         </v-col>
       </v-row>
     </v-col>
-
   </v-row>
 </template>
+
+<script>
+export default {
+  name: 'Post',
+
+  data: () => ({
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+ <Post />
+</template>
+
+<script>
+import Post from '../components/Post.vue';
+
+export default {
+  name: 'Home',
+
+  components: {
+    Post,
+  },
+};
+</script>
