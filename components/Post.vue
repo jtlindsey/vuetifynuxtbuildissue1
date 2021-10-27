83941feb10f33cf597ea55a01c5f2cf1ad524a0f
Travis Lindsey <reachjtl@gmail.com>
2021-10-27 15:56:15 -0400

    initial commit

---
+++ components/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-row>
+        <v-img
+          class="col-12 col-md-6"
+          max-height="250px"
+          src="https://images.unsplash.com/photo-1518895949257-7621c3c786d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=688&q=80"
+        ></v-img> 
+
+        <v-col class="col-12 col-md-6">
+          <div>Post Title Example</div>
+        </v-col>
+      </v-row>
+    </v-col>
+
+  </v-row>
+</template>
